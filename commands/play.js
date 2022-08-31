6e53a6d4bbcd880bc7e1587bd4cdf8099b467761
tictac <fominsartems947@gmail.com>
2022-09-01 00:53:46 +0300

    migrate to render

--- commands/play.js
+++ commands/play.js
@@ -1,13 +1,18 @@
-const { SlashCommandBuilder } =  require ('@discordjs/builders');
-const getMusicInfo = require('../functions/getMusic.js');
+const { SlashCommandBuilder } = require("@discordjs/builders")
+const getMusicInfo = require("../functions/getMusic.js")
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('play')
-		.setDescription('playing a commissioned music or playlist from youtube or spotify'),
-	async execute(message,params) {
-        const {member,client: {distube}} = message;
-		if(!member.voice.channel) return message.reply("you are not in voice channel");
-		const {link} = await getMusicInfo(params)
-        distube.play(message, `${link}`)
+		.setName("play")
+		.setDescription("playing a commissioned music or playlist from youtube or spotify"),
+	async execute(message, params) {
+		const {
+			member,
+			client: { distube },
+		} = message
+		if (!member.voice.channel) return message.reply("you are not in voice channel")
+		const { link } = await getMusicInfo(params)
+		distube.play(message.member.voice.channel, `${link}`, {
+			textChannel: message.channel,
+		})
 	},
-};
+}
--- index.js
+++ index.js
@@ -1,29 +1,28 @@
-
-const fs = require("fs");
-const { Client, Intents, Collection } = require("discord.js");
-const { DisTube } = require("distube");
-const { SpotifyPlugin } = require("@distube/spotify");
-require('dotenv').config()
+const fs = require("fs")
+const { Client, Intents, Collection } = require("discord.js")
+const { DisTube } = require("distube")
+const { SpotifyPlugin } = require("@distube/spotify")
+require("dotenv").config()
 
 const client = new Client({
 	intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES],
-});
+})
 
-client.commands = new Collection();
-client.prefix = "?";
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+client.commands = new Collection()
+client.prefix = "?"
+const commandFiles = fs.readdirSync("./commands").filter((file) => file.endsWith(".js"))
 for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	client.commands.set(command.data.name, command);
+	const command = require(`./commands/${file}`)
+	client.commands.set(command.data.name, command)
 }
 
-const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+const eventFiles = fs.readdirSync("./events").filter((file) => file.endsWith(".js"))
 for (const file of eventFiles) {
-	const event = require(`./events/${file}`);
+	const event = require(`./events/${file}`)
 	if (event.once) {
-		client.once(event.name, (...args) => event.execute(...args));
+		client.once(event.name, (...args) => event.execute(...args))
 	} else {
-		client.on(event.name, (...args) => event.execute(...args));
+		client.on(event.name, (...args) => event.execute(...args))
 	}
 }
 const distube = new DisTube(client, {
@@ -32,34 +31,40 @@ const distube = new DisTube(client, {
 	leaveOnEmpty: false,
 	leaveOnFinish: false,
 	leaveOnStop: false,
-	plugins: [new SpotifyPlugin({
-		parallel: true,
-		emitEventsAfterFetching: false,
-		api: {
-			clientId: process.env.SPOTIFY_CLIENT_ID,
-			clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-		},
-	})]
-});
-const { MessageEmbed } = require('discord.js');
+	plugins: [
+		new SpotifyPlugin({
+			parallel: true,
+			emitEventsAfterFetching: false,
+			api: {
+				clientId: process.env.SPOTIFY_CLIENT_ID,
+				clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+			},
+		}),
+	],
+})
+const { MessageEmbed } = require("discord.js")
 distube
-	.on("playSong",(queue, song) => {
+	.on("playSong", (queue, song, ...other) => {
+		console.log(other)
 		const embedMessage = new MessageEmbed()
-			.setAuthor({ name: 'Now playing'})
-			.setTitle(song.name).setURL(song.url)
-			.addFields({ name: `\`${song.formattedDuration}\``, value:`\u200B` },)
-			.setFooter({text:`Requested by ${song.user.username}`})
-			queue.textChannel.send({ embeds: [embedMessage] });
+			.setAuthor({ name: "Now playing" })
+			.setTitle(song.name)
+			.setURL(song.url)
+			.addFields({ name: `\`${song.formattedDuration}\``, value: `\u200B` })
+			.setFooter({ text: `Requested by ${song.user.username}` })
+		queue.textChannel?.send({ embeds: [embedMessage] })
 	})
 	.on("addSong", (queue, song) => {
-		if (queue.songs.length <= 1) return;
-		queue.textChannel.send(`Added ${song.name} to the queue`);
+		console.log(queue)
+		if (queue.songs.length <= 1) return
+		queue.textChannel?.send(`Added ${song.name} to the queue`)
 	})
 	.on("addList", (queue, playlist) => {
-		queue.textChannel.send(`${playlist.name} was added to the queue`);
+		console.log(queue)
+		queue.textChannel?.send(`${playlist.name} was added to the queue`)
 	})
 	.on("error", (textChannel, e) => {
-		console.error(e);
-	});
-client.distube = distube;
-client.login(process.env.DISCORD_TOKEN);
+		console.error(e)
+	})
+client.distube = distube
+client.login(process.env.DISCORD_TOKEN)
